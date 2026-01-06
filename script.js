a2c084731d4c4c27c146959757105c98e54e4b08
BrentCoder13 <flores_calvinbrent@spcc.edu.ph>
2026-01-07 01:52:13 +0800

    Initial commit with updated navbar and pages

---
+++ script.js
@@ -0,0 +1,26 @@
+const openAuthBtn = document.querySelector(".open-auth");
+const bookNowBtn = document.querySelector("#bookNowBtn"); // select Book Now button
+const modal = document.getElementById("authModal");
+const closeModalBtn = document.getElementById("closeModal");
+
+// open modal on Register/Login button click
+openAuthBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+// open modal on Book Now button click
+bookNowBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+// close modal
+closeModalBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+// close when clicking outside modal box
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("active");
+  }
+});
